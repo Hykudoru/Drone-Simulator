2975987fc6cc20b909a357c4f3e70a786074e6f8
Hykudoru <iamalexfish@gmail.com>
2020-12-05 04:40:02 -0600

    Drone Profile

---
+++ Drones/Assets/Drones/DroneProfile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Drone", fileName = "Drone")]
+public class DroneProfile : ScriptableObject
+{
+    public float maxMoveSpeed = 70f;
+    [Range(0f, 2f)] public float timeReachMaxMoveSpeed;
+    public float maxThrottleSpeed = 50f;
+    [Range(0f, 2f)] public float timeReachMaxThrottleSpeed;
+    public float maxYawSpeed = 360f;
+    [Range(0f, 2f)] public float timeReachMaxYawSpeed;
+    public float maxTilt = 28f;
+    [Range(0f, 2f)] public float timeReachMaxTilt;
+}

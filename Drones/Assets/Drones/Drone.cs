fc4b089c01b84eb9ec4bb067a82da04d4c612a03
Hykudoru <iamalexfish@gmail.com>
2020-12-14 14:21:39 -0600

    Propeller animation fixes and cleanup before 3.0

--- Drones/Assets/Drones/Drone.cs
+++ Drones/Assets/Drones/Drone.cs
@@ -3,37 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class Drone : MonoBehaviour
+public class Drone : MonoBehaviour //Flight Controller
 {
     Rigidbody drone;
     DroneInput input;
     Animator animator;
-    [SerializeField] float moveSpeed = 70f;
-    [SerializeField] float throttleSpeed = 50f;
-    [SerializeField] float yawSpeed = 360f;
-    [SerializeField] float tiltSpeed = 20f;
+    [SerializeField] DroneProfile specs;
+
+    [SerializeField] float maxMoveSpeed = 70f;
+    [SerializeField] float maxThrottleSpeed = 50f;
+    /* [SerializeField] float timeReachMaxSpeed = 0f;
+     [SerializeField] float timeReachZeroSpeed = 0f;
+     [SerializeField] float timeReachMaxThrottle = 0f;*/
+    [SerializeField] float maxYawSpeed = 360f;
+    [SerializeField] float maxTiltSpeed = 20f;
     [SerializeField] [Range(10, 60f)] float maxRollDeg = 28f;
     [SerializeField] [Range(10, 60f)] float maxPitchDeg = 28f;
     [SerializeField] [Range(10, 60f)] float maxTiltDeg = 28f;
     [SerializeField] bool counterGravity = true;
-    
+    [SerializeField] bool autoStabalizeOrientation = true;
+
     float roll;
     float pitch;
     float yaw;
-    Vector2 moveInput;
-    float throttleInput;
-    float yawInput;
-    
-    Vector3 move = Vector3.zero;
-    Vector3 throttle = Vector3.zero;
 
-    Vector3 thrust;
+
+    Vector3 changeInVelocity;
+    Vector3 thrust => drone.mass * changeInVelocity;
+
+    #region Experimenting
+    /*
+    //calculate required thrust to rotate drone upright
+    float tiltAcceleration = 1;
+    float upsideDownAngleAccel = 2;//if drone upside down or beyong
+    */
+
+    public bool IsUpsideDown() => Vector3.Dot(transform.up, Vector3.up) < 0f;
+    public float Orientation() => Vector3.Dot(transform.up, Vector3.up);
+    private void OnMouseOver()
+    {
+        Debug.Log(Orientation().ToString());
+    }
+    public float TiltRate => 0f;//if is upsidedown, increase tilt acceleration so drone rotates faster.
+
+    #endregion
+
+
 
     void Awake()
     {
         drone = GetComponent<Rigidbody>();
         input = new DroneInput();
         animator = GetComponent<Animator>();
+        if (specs)
+        {
+            Debug.Log(JsonUtility.ToJson(specs));
+        }
     }
 
     private void OnEnable()
@@ -45,44 +70,47 @@ public class Drone : MonoBehaviour
     {
         input.Drone.Disable();
     }
-    
+
     private void Update()
     {
-        moveInput = input.Drone.Move.ReadValue<Vector2>();
-        throttleInput = input.Drone.Throttle.ReadValue<float>();
-        yawInput = input.Drone.Rotate.ReadValue<float>();
+        Vector3 moveInput = input.Drone.Move.ReadValue<Vector2>();
+        float rotateInput = input.Drone.Rotate.ReadValue<float>();
+        float throttleInput = input.Drone.Throttle.ReadValue<float>();
 
-        yaw += yawInput * yawSpeed * Time.deltaTime;
+        yaw += rotateInput * maxYawSpeed * Time.deltaTime;
         roll = moveInput.x * maxRollDeg;
         pitch = moveInput.y * maxPitchDeg;
 
-        move = (drone.transform.right * moveInput.x + transform.forward * moveInput.y) * moveSpeed;
-        move.y = 0;
-        throttle = drone.transform.up * throttleInput * throttleSpeed;
+        changeInVelocity = (drone.transform.right * moveInput.x + transform.forward * moveInput.y) * maxMoveSpeed;
+        changeInVelocity.y = 0;
+        changeInVelocity += throttleInput  * drone.transform.up * maxThrottleSpeed;
 
-        thrust = move + throttle;
         // cancel gravity
         if (counterGravity && drone.useGravity)
         {
-            thrust += -Physics.gravity;// * Time.deltaTime;
+            changeInVelocity += -Physics.gravity;
         }
     }
 
     private void FixedUpdate()
-    { 
+    {
         // roll/pitch/yaw
         Quaternion targetRotation = Quaternion.AngleAxis(yaw, Vector3.up)
             * Quaternion.AngleAxis(roll, -Vector3.forward)
             * Quaternion.AngleAxis(pitch, Vector3.right);
-        drone.transform.localRotation = Quaternion.Slerp(drone.transform.localRotation, targetRotation, tiltSpeed * Time.deltaTime);
+        drone.transform.localRotation = Quaternion.Slerp(drone.transform.localRotation, targetRotation, maxTiltSpeed * Time.deltaTime);
 
         //move up/down/left/right/forward/back
-        drone.velocity += thrust * Time.deltaTime;
+        drone.velocity += changeInVelocity * Time.deltaTime;
     }
+
+
 
     private void LateUpdate()
     {
-        float propellerSpeed = Mathf.Clamp(thrust.sqrMagnitude, 0f, 10f);
-        animator.SetFloat("Thrust", propellerSpeed);
+        
+        //float propellerSpeed = Mathf.Clamp(Vector3.(changeInVelocity.normalized, Vector3.one), 0f, 10f);
+        float propellerSpeed = Mathf.Clamp(changeInVelocity.sqrMagnitude*Time.deltaTime, -10f, 10f);
+        animator.SetFloat("PropellerSpeed", propellerSpeed);
     }
 }
